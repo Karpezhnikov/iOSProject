16dd6a558f8ae8ab279a33103a44c8f03fdc6a79
Karpezhnikov <47120094+Karpezhnikov@users.noreply.github.com>
2019-11-12 11:41:28 +0300

    +

---
+++ Flamingo/Flamingo/iBuillder/StorageManager.swift
@@ -0,0 +1,32 @@
+//
+//  StorageManager.swift
+//  Flamingo
+//
+//  Created by Алексей Карпежников on 28/10/2019.
+//  Copyright © 2019 Алексей Карпежников. All rights reserved.
+//
+// для работы с Realm
+
+import RealmSwift
+
+let schemaVersion: UInt64 = 2
+let config = Realm.Configuration(
+        // Get the URL to the bundled file
+    fileURL: Bundle.main.url(forResource: "default", withExtension: "realm"),
+    readOnly: true,
+    schemaVersion: schemaVersion)
+
+// Open the Realm with the configuration
+let realm = try! Realm(configuration: config)
+
+
+class StorageManager{
+    
+    // сохраняет полученный объект
+    static func saveObject(_ service: Service){
+        try! realm.write {
+            realm.add(service)
+        }
+    }
+    
+}
